207fba7d5d2a5802dc2621e412211268d67d2983
AlexIngGuerra <39949921+AlexIngGuerra@users.noreply.github.com>
2020-10-23 23:28:40 -0600

    Cambio el acceso a la base de datos

--- server/src/routes/indexroutes.ts
+++ server/src/routes/indexroutes.ts
@@ -3,12 +3,11 @@ var jwt = require ('jsonwebtoken');
 var mssql = require('mssql');
 
 var config = {
-    server: 'localhost', //update me
-    //database: 'PELICULAS',  // Si hay falla de conexión se especifica la db a usar
+    server: 'localhost',
     host: 'localhost',
-    user: 'admsc',   // la base de datos a usar ya esta especificado en la config del usuario
-    password: 'Bu7n03Cc',
-    port: 1433
+    user: 'alexwgd', //Este debe ser su usuario
+    password: '1234', // Esta debe ser su contraseña
+    port: 1433 
 };
 
 class indexRoutes{
